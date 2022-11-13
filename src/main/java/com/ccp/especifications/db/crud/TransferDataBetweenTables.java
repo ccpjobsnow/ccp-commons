8309cf208990c0fe0be8c0432da9fdeb5479450a
onias <onias@noxxonsat.com.br>
2022-11-13 02:47:22 -0300

    Logout, UnlockToken

---
+++ src/main/java/com/ccp/especifications/db/crud/TransferDataBetweenTables.java
@@ -0,0 +1,36 @@
+package com.ccp.especifications.db.crud;
+
+import com.ccp.decorators.CcpMapDecorator;
+import com.ccp.especifications.db.table.CcpDbTable;
+import com.ccp.process.CcpNextStep;
+import com.ccp.process.CcpStepResult;
+
+public class TransferDataBetweenTables extends CcpNextStep {
+
+	private final CcpDbTable origin;
+	private final CcpDbTable target;
+	
+	public TransferDataBetweenTables(CcpDbTable origin, CcpDbTable target) {
+		this.origin = origin;
+		this.target = target;
+	}
+
+	@Override
+	public CcpStepResult executeThisStep(CcpMapDecorator values) {
+		
+		CcpMapDecorator tables = values.getInternalMap("_tables");
+
+		boolean doNothing = tables.getInternalMap(this.origin.name()).isEmpty() ;
+		
+		if(doNothing) {
+			return new CcpStepResult(values, 200, this);
+		}
+		
+		CcpMapDecorator remove = this.origin.remove(values);
+		this.target.save(remove);
+		CcpMapDecorator renameKey = tables.renameKey(this.origin.name(), this.target.name());
+		CcpMapDecorator put = values.put("_tables", renameKey);
+		return new CcpStepResult(put, 200, this);
+	}
+
+}
