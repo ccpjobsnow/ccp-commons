c2a659ecadf2bc91c866f57003c6eedcc1d3d341
onias <onias@noxxonsat.com.br>
2023-01-01 16:13:25 -0300

    enviar mensagens por email

--- src/main/java/com/ccp/especifications/email/CcpEmailSender.java
+++ src/main/java/com/ccp/especifications/email/CcpEmailSender.java
@@ -1,21 +1,21 @@
 package com.ccp.especifications.email;
 
 import com.ccp.decorators.CcpMapDecorator;
+import com.ccp.especifications.http.CcpHttpResponse;
 
 public interface CcpEmailSender {
 
 	void send(CcpMapDecorator emailParameters) ;
 	
 	@SuppressWarnings("serial")
-	public static class EmailWasNotSent extends RuntimeException {
-		public final String errorDetails;
+	public static class ThereWasClientError extends RuntimeException {
+		public final String message;
 
-		public EmailWasNotSent(String errorDetails) {
-			this.errorDetails = errorDetails;
+		public ThereWasClientError(CcpHttpResponse httpResponse) {
+			this.message = httpResponse.httpResponse;
 		}
-		
-		
-		
 	}
 	
+	@SuppressWarnings("serial")
+	public static class EmailApiIsUnavailable extends RuntimeException{}
 }
