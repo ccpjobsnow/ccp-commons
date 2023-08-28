ceb561dc683a2c1c071328be69c4618aa29b7140
onias <onias@noxxonsat.com.br>
2023-08-28 19:56:56 -0300

    mensagem qualquer

--- src/main/java/com/ccp/especifications/http/CcpHttpRequester.java
+++ src/main/java/com/ccp/especifications/http/CcpHttpRequester.java
@@ -4,4 +4,10 @@ import com.ccp.decorators.CcpMapDecorator;
 
 public interface CcpHttpRequester {
 	CcpHttpResponse executeHttpRequest(String url, String method, CcpMapDecorator headers, String body);
+
+	default CcpHttpResponse executeHttpRequest(String url, String method, CcpMapDecorator headers, String body, int expectedStatus) {
+		CcpHttpResponse res = this.executeHttpRequest(url, method, headers, body);
+		res.assertStatus(expectedStatus);
+		return res;
+	}
 }
