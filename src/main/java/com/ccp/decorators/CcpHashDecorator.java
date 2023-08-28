5a4a2bb3cf1e154865bd5693d0b6fa74036d4b4e
onias <onias@noxxonsat.com.br>
2023-08-28 16:08:09 -0300

    mensagem qualquer

--- src/main/java/com/ccp/decorators/CcpHashDecorator.java
+++ src/main/java/com/ccp/decorators/CcpHashDecorator.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class CcpHashDecorator {
 	public final String content;
@@ -58,13 +59,22 @@ public class CcpHashDecorator {
 
 		return strHash;
 	}
-	public BigInteger asBigInteger(String algorithm) {
-		MessageDigest digest;
+	
+	private static HashMap<String, MessageDigest> messageDigests = new HashMap<>();
+	static {
 		try {
-			digest = MessageDigest.getInstance(algorithm);
+			messageDigests.put("MD5", MessageDigest.getInstance("MD5"));
+			messageDigests.put("SHA1", MessageDigest.getInstance("SHA1"));
+			messageDigests.put("SHA-256", MessageDigest.getInstance("SHA-256"));
+			messageDigests.put("SHA-512", MessageDigest.getInstance("SHA-512"));
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	public BigInteger asBigInteger(String algorithm) {
+		MessageDigest digest = messageDigests.get(algorithm);
+		
 		byte[] bytes = this.content.getBytes(StandardCharsets.UTF_8);
 		byte[] hash = digest.digest(bytes);
 		BigInteger bi = new BigInteger(hash);
