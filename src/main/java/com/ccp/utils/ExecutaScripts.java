3b874727ac40dac9fb81e9e6e6ed010571312f93
J4550N51LV4 <jasson@noxxonsat.com.br>
2023-12-12 20:48:33 -0300

    Automação do Git

---
+++ src/main/java/com/ccp/utils/ExecutaScripts.java
@@ -0,0 +1,41 @@
+package com.ccp.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.swing.JOptionPane;
+
+public class ExecutaScripts {
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		String mensagemDeCommit = JOptionPane.showInputDialog("Digite sua mensagem de commit.");
+		StringBuilder respostaDoAdd = executarComando("git add .");
+		System.out.println(respostaDoAdd);
+		String command = "git commit -m '"
+				+ mensagemDeCommit
+				+ "'";
+		StringBuilder respostaDoCommit = executarComando(command);
+		System.out.println(respostaDoCommit);
+		StringBuilder respostaDoPush = executarComando("git push");
+		System.out.println(respostaDoPush);
+		
+	}
+
+	private static StringBuilder executarComando(String command) throws IOException {
+		Runtime runtime = Runtime.getRuntime();
+		Process proc = runtime.exec(command);
+		InputStream stdIn = proc.getInputStream();
+		InputStreamReader isr = new InputStreamReader(stdIn);
+		BufferedReader br = new BufferedReader(isr);
+
+		String line = null;
+		StringBuilder sb = new StringBuilder(); 
+
+		while ((line = br.readLine()) != null) {
+			sb.append(line).append("\n");
+		}
+		return sb;
+	}
+}
